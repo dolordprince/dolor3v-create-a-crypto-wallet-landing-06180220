6b97adb771445d365c5b8c1c2e82e0298ef11f1c
dolordprince <personaldolor@gmail.com>
2026-06-18 02:21:04 +0000

    DOLOR3V build

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to the get started button
+document.querySelector('button').addEventListener('click', () => {
+  // Redirect to the about section
+  window.location.href = '#about';
+});
